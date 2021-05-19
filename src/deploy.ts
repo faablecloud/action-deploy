5a1db0a5960dee8e9165ac83dd8968b326880da1
Marc Pomar <marc@corecode.school>
2021-05-19 17:39:26 +0200

    fix: done

--- src/deploy.ts
+++ src/deploy.ts
@@ -10,12 +10,24 @@ const copy_files = () => {
   copySync(`${templates}/entrypoint.sh`, `${dst}/entrypoint.sh`);
 };
 
+type FaableContext = {
+  faable_app_name: string;
+  faable_api_key: string;
+};
+
+const get_context = (): FaableContext => {
+  return {
+    faable_app_name: core.getInput("faable_app_name"),
+    faable_api_key: core.getInput("faable_api_key"),
+  };
+};
+
 const main = () => {
   console.log("Building docker image");
   copy_files();
   console.log("Files copied");
-  const app_name = core.getInput("app_name");
-  const tag = `registry.faable.com/${app_name}`;
+  const ctx = get_context();
+  const tag = `registry.faable.com/${ctx.faable_app_name}`;
 
   // Execute build
   execSync("ls -als", { stdio: "inherit" });
@@ -23,7 +35,7 @@ const main = () => {
   execSync(`docker images`, { stdio: "inherit" });
 
   core.setOutput("status", "✅ Successfully deployed to FaableCloud");
-  core.setOutput("status", `✅ https://${app_name}.app.faable.com`);
+  core.setOutput("status", `✅ https://${ctx.faable_app_name}.app.faable.com`);
 };
 
 main();
