6b95d9215168d1ae61401e176aaacda2f2153531
Marc Pomar <marc@corecode.school>
2021-05-19 17:30:22 +0200

    build changes

--- dist/deploy.js
+++ dist/deploy.js
@@ -6,13 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const fs_extra_1 = require("fs-extra");
 const path_1 = __importDefault(require("path"));
+const core_1 = __importDefault(require("@actions/core"));
+const copy_files = () => {
+    const templates = path_1.default.join(__dirname, "..", "templates");
+    const dst = path_1.default.join(process.cwd(), "Dockerfile");
+    fs_extra_1.copySync(`${templates}/Dockerfile.template`, `${dst}/Dockerfile`);
+    fs_extra_1.copySync(`${templates}/entrypoint.sh`, `${dst}/entrypoint.sh`);
+};
 const main = () => {
     console.log("Building docker image");
-    const dockerfile_src = path_1.default.join(__dirname, "..", "templates/Dockerfile.template");
-    const dockerfile_dst = path_1.default.join(process.cwd(), "Dockerfile");
-    console.log("Copy", dockerfile_src, dockerfile_dst);
-    fs_extra_1.copySync(dockerfile_src, dockerfile_dst);
+    copy_files();
+    console.log("Files copied");
+    const app_name = core_1.default.getInput("app_name");
+    const tag = `registry.faable.com/${app_name}`;
     child_process_1.execSync("ls -als", { stdio: "inherit" });
-    child_process_1.execSync("docker build .", { stdio: "inherit" });
+    child_process_1.execSync(`docker build -t ${tag} .`, { stdio: "inherit" });
+    child_process_1.execSync(`docker images`, { stdio: "inherit" });
 };
 main();
