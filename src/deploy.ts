f1ae6a10ab6a87c6dafed61eb61ef1bdd7e1f282
Marc Pomar <marc@corecode.school>
2021-05-19 17:27:37 +0200

    fix: action

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,19 +1,24 @@
 import { execSync } from "child_process";
 import { copySync } from "fs-extra";
 import path from "path";
+import core from "@actions/core";
+
+const copy_files = () => {
+  const templates = path.join(__dirname, "..", "templates");
+  const dst = path.join(process.cwd(), "Dockerfile");
+  copySync(`${templates}/Dockerfile.template`, `${dst}/Dockerfile`);
+  copySync(`${templates}/entrypoint.sh`, `${dst}/entrypoint.sh`);
+};
 
 const main = () => {
   console.log("Building docker image");
-  const dockerfile_src = path.join(
-    __dirname,
-    "..",
-    "templates/Dockerfile.template"
-  );
-  const dockerfile_dst = path.join(process.cwd(), "Dockerfile");
-  console.log("Copy", dockerfile_src, dockerfile_dst);
-  copySync(dockerfile_src, dockerfile_dst);
+  copy_files();
+  console.log("Files copied");
+  const app_name = core.getInput("app_name");
+  const tag = `registry.faable.com/${app_name}`;
   execSync("ls -als", { stdio: "inherit" });
-  execSync("docker build .", { stdio: "inherit" });
+  execSync(`docker build -t ${tag} .`, { stdio: "inherit" });
+  execSync(`docker images`, { stdio: "inherit" });
 };
 
 main();
