bd574e40cf56edd1612c98540dc3f6150a345124
Marc Pomar <marc@corecode.school>
2021-05-19 17:15:55 +0200

    fix: build deploy

--- dist/deploy.js
+++ dist/deploy.js
@@ -1,6 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
-child_process_1.execSync("ls -als", { stdio: "inherit" });
-child_process_1.execSync("docker ps", { stdio: "inherit" });
-console.log("HAHA");
+const fs_extra_1 = require("fs-extra");
+const path_1 = __importDefault(require("path"));
+const main = () => {
+    console.log("Building docker image");
+    const dockerfile = path_1.default.join(__dirname, "..", "templates/Dockerfile");
+    fs_extra_1.copySync(dockerfile, process.cwd());
+    console.log("Copied to ", dockerfile, process.cwd());
+    child_process_1.execSync("ls -als", { stdio: "inherit" });
+    child_process_1.execSync("docker build .", { stdio: "inherit" });
+};
+main();
--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,14 @@
 import { execSync } from "child_process";
+import { copySync } from "fs-extra";
+import path from "path";
 
-execSync("ls -als", { stdio: "inherit" });
-execSync("docker ps", { stdio: "inherit" });
+const main = () => {
+  console.log("Building docker image");
+  const dockerfile = path.join(__dirname, "..", "templates/Dockerfile");
+  copySync(dockerfile, process.cwd());
+  console.log("Copied to ", dockerfile, process.cwd());
+  execSync("ls -als", { stdio: "inherit" });
+  execSync("docker build .", { stdio: "inherit" });
+};
 
-console.log("HAHA");
+main();
