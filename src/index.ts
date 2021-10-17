e57102b84b7325c9da29bc38e9d4d9b2826e6bf1
Marc Pomar <marc@corecode.school>
2021-10-17 19:13:21 +0200

    fix: done

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import { deploy_action } from "./deploy/deploy_action";
 import { get_context } from "./lib/FaableContext";
 import pkg from "../package.json";
 
-const main = () => {
+const main = async () => {
   console.log(`🚀 FaableCloud ${pkg.name} - ${pkg.version}`);
   const ctx = get_context();
   try {
-    deploy_action(ctx);
+    await deploy_action(ctx);
   } catch (error) {
     console.log(error);
     process.exit(-1);
   }
 };
+
 main();
