82a3749ae8d4b318c206616b720e5a366c3bb687
Marc Pomar <marc@corecode.school>
2022-11-22 11:38:17 +0100

    fix: build

---
+++ rollup.config.mjs
@@ -0,0 +1,11 @@
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    file: "dist/bundle.js",
+    format: "cjs",
+  },
+  plugins: [json(), typescript()],
+};
